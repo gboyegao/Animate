215ff15a87df76557f385f96ba80e9749925bb23
Adegboyega Olusunmade <gboyegaolusunmade@gmail.com>
2019-09-11 15:58:03 +0100

    Initial Commit

---
+++ Animate/Animate.h
@@ -0,0 +1,19 @@
+//
+//  Animate.h
+//  Animate
+//
+//  Created by Adegboyega Olusunmade on 11/09/2019.
+//  Copyright © 2019 Adegboyega Olusunmade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Animate.
+FOUNDATION_EXPORT double AnimateVersionNumber;
+
+//! Project version string for Animate.
+FOUNDATION_EXPORT const unsigned char AnimateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Animate/PublicHeader.h>
+
+
