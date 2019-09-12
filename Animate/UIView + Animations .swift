6e0f7129453acc7eff90ba1d6ff43c6daab69b46
Adegboyega Olusunmade <gboyegaolusunmade@gmail.com>
2019-09-12 11:20:18 +0100

    Initial Commit

---
+++ Animate/UIView + Animations .swift
@@ -0,0 +1,9 @@
+//
+//  UIView + Animations .swift
+//  Animate
+//
+//  Created by Adegboyega Olusunmade on 12/09/2019.
+//  Copyright © 2019 Adegboyega Olusunmade. All rights reserved.
+//
+
+import Foundation
